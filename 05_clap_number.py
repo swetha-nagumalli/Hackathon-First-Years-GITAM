a72d1ff89d3555056e878bc720c951c915db5853
swetha-nagumalli <78730543+swetha-nagumalli@users.noreply.github.com>
2021-03-07 15:01:07 +0530

    Update 05_clap_number.py

--- 05_clap_number.py
+++ 05_clap_number.py
@@ -23,7 +23,19 @@ Explanation:
 # Implement the below function and run the program
 
 def clap_number(n):
-    pass
+     string = "clap"
+    ls=[str(i) for i in range(1,n+1)]
+    for i in range(len(ls)):
+        if int(ls[i])%3==0:
+            ls[i]=string
+        elif '3' in ls[i]:
+            ls[i]=string
+        elif '6' in ls[i]:
+            ls[i]=string
+        elif '9' in ls[i]:
+            ls[i]=string
+    return ls
+     
 
 
 class TestClapNumber(unittest.TestCase):
